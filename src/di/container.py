ec7118790c81e1ca28c976c048e7686bf4d480cb
TSELOBANOV Bogdan V <bogdan.tselobanov@raiffeisen.ru>
2026-06-24 17:47:02 +0300

    module 5 - di

--- src/di/container.py
+++ src/di/container.py
@@ -1,23 +1,10 @@
-from that_depends import BaseContainer
+from that_depends import BaseContainer, Provider
+
+from src.core.domain.services.dispatch_service import DispatchDomainService
 
 
 class Container(BaseContainer):
-    """
-    DI container placeholder.
-    
-    Add your own dependencies here as needed.
-    Example:
-    
-    from that_depends import Factory
-    from adapters.out.postgres.database import async_session_factory
-    from adapters.out.postgres.courier_repository import CourierRepository
-    
-    @staticmethod
-    async def courier_repository(session_factory) -> CourierRepository:
-        return CourierRepository(session_factory)
-    """
-
-    pass
+    dispatch_domain_service = Provider(DispatchDomainService)
 
 
 container = Container()
