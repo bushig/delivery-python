f0a8fccd5c19ac6ed84c4b36fe2b31e1bab6f399
TSELOBANOV Bogdan V <bogdan.tselobanov@raiffeisen.ru>
2026-06-27 15:51:02 +0300

    module 7 get all couriers

--- src/core/application/queries/get_all_couriers.py
+++ src/core/application/queries/get_all_couriers.py
@@ -1,15 +1,27 @@
 
+from dataclasses import dataclass
 from uuid import UUID
 
 from src.core.domain.model.location import Location
+from src.core.domain.ports import UnitOfWork
 
 
+@dataclass(frozen=True, slots=True, kw_only=True)
 class CourierResponseDTO:
     id: UUID
     name: str
     location: Location
 
-def get_all_couriers() -> list[CourierResponseDTO]:
-    # Получить всех курьеров из БД
 
-    pass
+async def get_all_couriers(uow: UnitOfWork) -> list[CourierResponseDTO]:
+    async with uow:
+        couriers = await uow.couriers.get_all()
+
+    return [
+        CourierResponseDTO(
+            id=courier.id,
+            name=courier.name,
+            location=courier.location,
+        )
+        for courier in couriers
+    ]
